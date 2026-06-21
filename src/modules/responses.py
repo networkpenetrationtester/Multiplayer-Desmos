09e91a5cab0f3dcd9b454729c570fd120788f5c4
Neo <77405346+networkpenetrationtester@users.noreply.github.com>
2026-06-21 00:16:06 -0500

    feat: migration to standard API response

--- src/modules/responses.py
+++ src/modules/responses.py
@@ -1,67 +1,25 @@
 from enum import Enum
 
-from src.modules.objects import APIResonse
+from pydantic import BaseModel
+from typing_extensions import Any
+
+
+class APIResonse(BaseModel):
+    success: bool
+    message: str | None = None
+    data: dict[str, Any] | None = None
 
 
 class FAILURE_CODES(Enum):
     GENERAL = 0
-    USERNAME_VOID = 1
-    USERNAME_EXISTS = 2
-    USERNAME_INVALID = 3
-    EMAIL_VOID = 4
-    EMAIL_EXISTS = 5
-    EMAIL_INVALID = 6
+    USER_VOID = 1
+    USER_EXISTS = 2
+    USER_INVALID = 3
+    # EMAIL_VOID = 4
+    # EMAIL_EXISTS = 5
+    # EMAIL_INVALID = 6
     PASSWORD_INVALID = 7
     GRAPH_VOID = 8
     GRAPH_EXISTS = 9
     GRAPH_INVALID = 10
-
-
-class CreateUserResponse(APIResonse):
-    auth_token: str
-    refresh_token: str
-
-
-class ReadUserResponse(APIResonse):
-    auth_token: str
-    refresh_token: str
-
-
-class UpdateUserResponse(APIResonse):
-    pass
-
-
-class DeleteUserResponse(APIResonse):
-    pass
-
-
-class CreateRoomResponse(APIResonse):
-    pass
-
-
-class ReadRoomsResponse(APIResonse):
-    pass
-
-
-class UpdateRoomResponse(APIResonse):
-    pass
-
-
-class DeleteRoomResponse(APIResonse):
-    pass
-
-
-class CreateGraphResponse(APIResonse):
-    pass
-
-
-class ReadGraphsResponse(APIResonse):
-    pass
-
-
-class UpdateGraphResponse(APIResonse):
-    pass
-
-
-class DeleteGraphResponse(APIResonse):
-    pass
+    TOKEN_INVALID = 11
