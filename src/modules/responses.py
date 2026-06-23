663d84f32dde84553638e17abb8e900e94dc1775
Neo <77405346+networkpenetrationtester@users.noreply.github.com>
2026-06-23 00:08:54 -0500

    feat: new response codes + refactor: move API response here

--- src/modules/responses.py
+++ src/modules/responses.py
@@ -22,4 +22,24 @@ class FAILURE_CODES(Enum):
     GRAPH_VOID = 8
     GRAPH_EXISTS = 9
     GRAPH_INVALID = 10
-    TOKEN_INVALID = 11
+    TOKEN_VOID = 11
+    TOKEN_INVALID = 12
+
+
+def APIResponse(
+    success: bool,
+    failure_code: FAILURE_CODES | None = None,
+    message: str | None = None,
+    data: dict[str, Any] | None = None,
+):
+
+    _dict = dict()
+    _dict["success"] = success
+    if failure_code:
+        _dict["failure_code"] = failure_code
+    if message:
+        _dict["message"] = message
+    if data:
+        _dict["data"] = data
+
+    return _dict
